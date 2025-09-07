c2d3875b531071cfe5b2b765e7d0c0fc414f1f42
Ashish_Dhakad444 <136087464+AshishDhakad@users.noreply.github.com>
2025-09-07 23:41:20 +0530

    square_pattern

---
+++ Square_Star_Pattern..js
@@ -0,0 +1,18 @@
+
+// *****
+// *****
+// *****
+// *****
+// *****
+
+const n=5
+
+for(let i=1;i<=n;i++)
+{
+    let st=""
+    for(let j=1;j<=n;j++)
+    {
+        st+="*"
+    }
+    console.log(st)
+}
