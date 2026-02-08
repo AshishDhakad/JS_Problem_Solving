557377d619d3824d84bd2f668c760ba0033cf487
Ashish_Dhakad444 <ashishdhakad685@gmail.com>
2026-02-08 23:54:15 +0530

    second approach

--- Basic_Question/removeDuplicate.js
+++ Basic_Question/removeDuplicate.js
@@ -2,5 +2,12 @@
 
 const arr =[1,23,3,4,5,1,3,3,4,4,5,56,6,7,78,9]
 
-const res = [...new Set(arr)];
-console.log(res);
+
+//   first approach 
+// const res = [...new Set(arr)];
+// console.log(res);
+
+// Second approach
+const newRes = arr.filter((ele,idx)=>arr.indexOf(ele)===idx)
+console.log(newRes);
+
