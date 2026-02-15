0b2d5cb1d4147041468e71e0563635974b5c489e
Ashish_Dhakad444 <ashishdhakad685@gmail.com>
2026-02-15 23:46:32 +0530

    Brute Force Algorithm

--- Basic_Question/removeDuplicate.js
+++ Basic_Question/removeDuplicate.js
@@ -1,6 +1,17 @@
 
 
 const arr =[1,23,3,4,5,1,3,3,4,4,5,56,6,7,78,9]
+let res =[]
+
+for(let i=0;i<arr.length;i++){
+    let flag =false;
+    for(let j=0;j<res.length;j++)
+    {
+      if(arr[i]==res[j]) flag=true;
+    }
+    if(!flag) res.push(arr[i]);
+}
+console.log(res);
 
 
 //   first approach 
@@ -8,6 +19,6 @@ const arr =[1,23,3,4,5,1,3,3,4,4,5,56,6,7,78,9]
 // console.log(res);
 
 // Second approach
-const newRes = arr.filter((ele,idx)=>arr.indexOf(ele)===idx)
-console.log(newRes);
+// const newRes = arr.filter((ele,idx)=>arr.indexOf(ele)===idx)
+// console.log(newRes);
 
